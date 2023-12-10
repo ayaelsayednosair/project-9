170dfeeaa0d02a5363361ab680bb5f835032f04f
ayaalsaid <140819543+ayaalsaid@users.noreply.github.com>
2023-12-10 05:23:00 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Housinglocation } from './../housinglocation';
+import { Component, inject } from '@angular/core';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: Housinglocation[] = [];
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
